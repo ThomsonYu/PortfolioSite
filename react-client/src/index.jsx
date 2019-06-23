7fd54e3dd4d2fc8566f98867a12139a002a821a8
Thomson <thomson.yu@mail.utoronto.ca>
2019-06-23 16:48:06 -0400

    Added footer component

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'; // Allows us to use JSX html in our javascript code
+import ReactDOM from 'react-dom'; // Allows us to render elements
 import App from './Components/App';
 
+// ReactDOM.render(What to Render, Where to Render)
 ReactDOM.render(<App />, document.getElementById('app'));
