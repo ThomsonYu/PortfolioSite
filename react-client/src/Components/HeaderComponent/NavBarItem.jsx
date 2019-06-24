6f66c96bd75fb8538a34d8fa8ffe78c1d4d6552a
Thomson <thomson.yu@mail.utoronto.ca>
2019-06-24 17:48:48 -0400

    class based component for nav bar item

--- react-client/src/Components/HeaderComponent/NavBarItem.jsx
+++ react-client/src/Components/HeaderComponent/NavBarItem.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-function NavBarItem(props){
-    return (
-        <li className="nav-item btn">
-            <a className="nav-link" href={props.link}>{props.name}</a>
-        </li>
-    )
+class NavBarItem extends Component{
+    render(){
+        return(
+            <li className="nav-item btn">
+                <a className="nav-link" href={this.props.link}>{this.props.name}</a>
+            </li>
+        )
+    }
 }
 
 export default NavBarItem;
